cd8d42addce4b39f8214610738f43f5cee6851b5
Ibrahim-Jarada <ibrahim.jarada@gmail.com>
2022-03-01 19:37:52 +0200

    main structure + main functionality Relates #4

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+const signInForm =document.querySelector('.sign-in-form');
+const signUpForm =document.querySelector('.sign-up-form');
+const signInUpToggleBtn=document.querySelector('.sign-in-up');
+
+const toggleContent = content=>content==='New User?'?'Sign in?':'New User?';
+signInUpToggleBtn.addEventListener('click',()=>{
+    signInUpToggleBtn.textContent=toggleContent(signInUpToggleBtn.textContent);
+    signInForm.classList.toggle('disable');
+    signUpForm.classList.toggle('disable');
+})
+
+signInForm.addEventListener('submit',e=>{
+    e.preventDefault();
+})
+
+signUpForm.addEventListener('submit',e=>{
+    e.preventDefault();
+})
