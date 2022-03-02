f59f04f81ff6da7328bbce793591c425fbca3681
Ibrahim-Jarada <ibrahim.jarada@gmail.com>
2022-03-02 11:38:37 +0200

    add bill route functionality Relates #17

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,8 @@
 const { userErrorHandel, serverErrorHandel } = require('./error');
 const { signUp } = require('./sign-up');
+const { addBill } = require('./add-bill');
+const { signIN } = require('./sign-in');
 
-module.exports = { userErrorHandel, serverErrorHandel, signUp };
+module.exports = {
+  userErrorHandel, serverErrorHandel, signUp, addBill, signIN,
+};
--- src/route/index.js
+++ src/route/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
-const { userErrorHandel, serverErrorHandel, signUp } = require('../controllers');
+const {
+  userErrorHandel, serverErrorHandel, signUp, addBill, signIN,
+} = require('../controllers');
 
-router.post('/sign-in', userErrorHandel);
+router.post('/sign-in', signIN);
 router.post('/sign-up', signUp);
-router.post('/add-bill', userErrorHandel);
+router.post('/add-bill', addBill);
 router.post('/delete-bill', userErrorHandel);
 
 router.use(userErrorHandel);
