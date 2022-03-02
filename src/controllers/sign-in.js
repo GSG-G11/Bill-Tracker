e3cd15fdfeb8c1c9491fa768246799fce3541799
MahmoudJD95 <mld95.doa@gmail.com>
2022-03-02 10:04:34 +0200

    create sign in function >relates #15

--- src/controllers/sign-in.js
+++ src/controllers/sign-in.js
@@ -1,11 +1,29 @@
 const connection = require('../database/config');
 
 const signIN = (req, res) => {
-  const { bb } = req.body;
-  return connection.query({
-    text: 'SELECT * FROM users WHERE username=$1 RETURNNING *;',
-    values: [bb],
-  }).then(res.json);
+  const { userName, password } = req.body;
+  if (userName && password) {
+    return connection.query({
+      text: 'SELECT * FROM users WHERE user_name=$1 AND password=$2  RETURNING *;',
+      values: [userName, password],
+    })
+      .then((data) => {
+        if (data.rows.length === 1) {
+          return res.json({
+            status: 'authenticated',
+            data: data.rows[0],
+          });
+        }
+        return res.json({
+          status: 'error',
+          message: 'wrong username or wrong password  ',
+        });
+      });
+  }
+  return res.json({
+    status: 'error',
+    message: 'invalid input',
+  });
 };
 
 module.exports = signIN;
